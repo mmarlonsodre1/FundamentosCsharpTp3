467169e4346b83acfe6002d770dc86bf96ea81d2
Marlon Sodre <marlon.sodre@al.infnet.edu.br>
2020-06-20 19:12:09 -0300

    Init sql

---
+++ PersonRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public class PersonRepository
+{
+	private string ConnectionString { get; set; }
+
+	public PersonRepository(IConfigurationSystem configuration)
+	{
+		this.ConnectionString = configuration.GetConfig.ConnectionString("AtConnectionString");
+	}
+
+	public void Save(Person person)
+    {
+		using(var connection = new SqlConnection(this.ConnectionString))
+        {
+			if (connection.State != System.Data.ConnectionState.Open) connection.Open();
+			
+			SqlCommand sqlCommand = connection.CreateCommand();
+			sqlCommand.CommandText = "Insert into Person(Name, SurName, Birthday) values (@P1, @P2, @P3)";
+			sqlCommand.Parameters.AddWithValue("P1", person.Name);
+
+			sqlCommand.ExecuteNonQuery();
+			connection.Close();
+
+		}
+
+	}
+
+}
